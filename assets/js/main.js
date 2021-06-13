d95bf09cdbd3d41075f9b26141cb1002bd2f3158
Marcel <marcel.mulders@hotmail.com>
2021-06-13 15:58:42 +0100

    added menu selection for adding an item

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,14 @@ function setDefaults() {
     // Set default time
 
     // Load local data
-    let currentExerciseData = localStorage.getItem('exercise-data');
-    currentExerciseData = JSON.parse(currentExerciseData);
-    for (var itemNumber in currentExerciseData) {
-        let payload = currentExerciseData[itemNumber];
-        console.log(payload);
-        setItems(payload);
-    }
+    setItems();
 }
 
 function addItem() {
     // Get form data
     let date = document.getElementById('date').value;
     let time = document.getElementById('time').value;
-    let exercise = document.getElementById('exercise').value;
+    let exercise = document.getElementById('workout-exercise-value').value;
     let repetitions = document.getElementById('repetitions').value;
     // Validate form data
 
@@ -33,27 +27,36 @@ function addItem() {
     // Store data locally
     storeDataLocally(payload);
     // Add record
-    setItems(payload);
+    setItems();
 }
 
-function setItems(payload) {
+function setItems() {
+    let currentExerciseData = localStorage.getItem('exercise-data');
+    currentExerciseData = JSON.parse(currentExerciseData);
+    if (currentExerciseData == null) {return false;}
     let pastExercisesEl = document.getElementById('past-exercises');
-    pastExercisesEl.innerHTML += `<div class="exercise-set">
-    <p>Date-time: ` + payload.date.toString() + ' ' + payload.time.toString() + `</p>
-    <p>Exercise: ` + payload.exercise.toString() + `</p>
-    <p>Exercise: ` + payload.repetitions.toString() + `</p>
-    </div>`
+    pastExercisesEl.innerHTML = '';
+    for (let itemNumber in currentExerciseData.reverse()) {
+        let payload = currentExerciseData[itemNumber];
+        pastExercisesEl.innerHTML += `<div class="exercise-set">
+            <p>Date time: ` + payload.date.toString() + ' ' + payload.time.toString() + `</p>
+            <p>Exercise: ` + payload.exercise.toString() + `</p>
+            <p>Repetitions: ` + payload.repetitions.toString() + `</p>
+        </div>`;
+    }
 }
 
 function storeDataLocally(payload) {
     let currentExerciseData = localStorage.getItem('exercise-data');
     if (currentExerciseData == null) {
+        payload['id'] = 1;
         localStorage.setItem(
             'exercise-data',
             JSON.stringify([payload])
         );
     } else {
         currentExerciseData = JSON.parse(currentExerciseData);
+        payload['id'] = currentExerciseData.length + 1;
         currentExerciseData.push(payload);
         localStorage.setItem(
             'exercise-data',
@@ -62,7 +65,31 @@ function storeDataLocally(payload) {
     }
 }
 
+function setExerciseValue(exercise) {
+    document.getElementById('workout-exercise-value').value = exercise;
+    let activeExerciseListValue = exercise
+    if (exercise.length > 12) {
+        activeExerciseListValue = exercise.substring(0, 9) + '...'
+    }
+    document.getElementById('active-exercise-list-value').innerHTML = activeExerciseListValue;
+    document.getElementById('workout-exercise-list-pop-up').style.display = 'none';
+}
+
+function showExerciseList() {
+    document.getElementById('workout-exercise-list-pop-up').style.display = 'block';
+}
+
 function clearLocalExerciseData() {
     localStorage.removeItem('exercise-data');
     document.getElementById('past-exercises').innerHTML = '';
+}
+
+function showAddSessionMenu() {
+    console.log('show session menu clicked');
+    document.getElementById('exercise-form-section').style.display = 'block';
+}
+
+function closeAddSessionMenu() {
+    console.log('close session menu clicked');
+    document.getElementById('exercise-form-section').style.display = 'none';
 }
